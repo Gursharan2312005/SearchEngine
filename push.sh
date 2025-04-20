a6e39ecefe37422d3dcea205298835c3e0f05726
Gursharan2312005 <your.email@example.com>
2025-04-20 20:23:59 +0530

    Update render.yaml and push script

--- push.sh
+++ push.sh
@@ -13,8 +13,8 @@ git add .
 # Commit changes
 git commit -m "Update render.yaml and push script"
 
-# Set remote URL with authentication
-git remote set-url origin https://Gursharan2312005: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@github.com/Gursharan2312005/SearchEngine.git
+# Set remote URL with new authentication
+git remote set-url origin https://Gursharan2312005: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@github.com/Gursharan2312005/SearchEngine.git
 
 # Push to GitHub
 git push -u origin main 
